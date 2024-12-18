9b846482511ab9a1021f13d23cc886ced13e4dbf
Munshi91 <mimunshi91@gmail.com>
2024-12-18 12:35:25 +0600

    Add responssive

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  // Initialize Owl Carousel
+  $('.owl-carousel').owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: true,
+  });
+});
